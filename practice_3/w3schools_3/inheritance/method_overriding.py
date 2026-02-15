b4b85347071ae6bd53cc700745a1d3b4efea773d
Zeitoros <aituimash@gmail.com>
2026-02-15 18:44:10 +0500

    Method Overriding

---
+++ practice_3/w3schools_3/inheritance/method_overriding.py
@@ -0,0 +1,19 @@
+class Animal:
+    def speak(self):
+        print("Animal makes a sound")
+
+class Dog(Animal):
+    def speak(self):
+        print("Woof-woof!")
+
+class Cat(Animal):
+    def speak(self):
+        print("Meow!")
+
+animal = Animal()
+dog = Dog()
+cat = Cat()
+
+animal.speak()
+dog.speak()
+cat.speak()
