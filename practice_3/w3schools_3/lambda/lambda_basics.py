aceeb0f950318c8b3a73bc980c4a9d64d1a0691b
Zeitoros <aituimash@gmail.com>
2026-02-15 17:17:23 +0500

    Lambda basics

---
+++ practice_3/w3schools_3/lambda/lambda_basics.py
@@ -0,0 +1,11 @@
+# add = lambda a,b: a + b
+# a,b = map(int, input().split())
+# result = add(a,b)
+# print(result)
+
+
+# def smth(n):
+#     return lambda m: n**m
+
+# var = smth(3)
+# print(var(2))
